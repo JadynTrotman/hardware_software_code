a8d1246c883012bc7443bbe9378aa487119c00b7
JadynTrotman <jaytrot9@gmail.com>
2024-01-11 11:25:07 -0500

    1/11/2024 H&S class

--- my_conversation.py
+++ my_conversation.py
@@ -4,7 +4,10 @@ def conversation():
     if answer == "yes":
        print("That's good - the united states needs more coders!!")
     else:
+       if answer == "no":
         print("Perhaps you will change your mind")
+       else:
+        print("I do not understand your answer")
     print("Goodbye")
 
 def main():
---
+++ your_name.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What is your name")
+    print("We want to know if you like programming!")
+    answer = print("Do you like programming "+ name + "?")
+    answer = input()
+    print('Great! You said "' + answer + '"?')
+    print("Lets's start learning some Python today")
+if __name__ == "__main__":
+    main()
